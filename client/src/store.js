ec3535435ff3e476fd59e562b209d66e5297c62e
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-09-24 16:15:12 -0600

    axios

--- client/src/store.js
+++ client/src/store.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
-let query = new random ([chicken, beef, dfasf, fsdfaf])
+// let query = new random ([chicken, beef,]) this will be for randomly generating our query in the baseUrl
 const _foodapi = axios.create({
   baseUrl: 'https://api.edamam.com/search?q=chicken&app_id=$e5152851app_key=$b5e235b115bbed4de5e65e86ac731f4a'
 })
