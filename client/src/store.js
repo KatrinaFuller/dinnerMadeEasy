cd522f1918358f26574c41b83574440d0b4b68a2
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-09-24 11:51:20 -0600

    api axios

--- client/src/store.js
+++ client/src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 let query = new random ([chicken, beef, dfasf, fsdfaf])
 const _foodapi = axios.create({
-  baseUrl: "//api.edamam.com/search?=" + query + "apikey" 
+  baseUrl: 'https://api.edamam.com/search?q=chicken&app_id=$e5152851app_key=$b5e235b115bbed4de5e65e86ac731f4a'
 })
 
 // const server = baseUrl: localhost 
