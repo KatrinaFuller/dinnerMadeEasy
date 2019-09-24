756f70763769d6dee5a5f0b478b5aa78a5f74d4f
Valerie Wiklund <vkwiklund@gmail.com>
2019-09-24 14:51:24 -0600

    changed authorId to userId

--- client/src/store.js
+++ client/src/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-let query = new random ([chicken, beef, dfasf, fsdfaf])
+// let query = new random ([chicken, beef, dfasf, fsdfaf])
 const _foodapi = axios.create({
   baseUrl: 'https://api.edamam.com/search?q=chicken&app_id=$e5152851app_key=$b5e235b115bbed4de5e65e86ac731f4a'
 })
