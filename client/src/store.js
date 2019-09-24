377762b73885704600b67747748522856ecc9f05
Stephanie Hooker <stephanieh799@gmail.com>
2019-09-24 15:55:20 -0600

    added AuthService file

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
+import AuthService from './AuthService'
 
 Vue.use(Vuex)
+
+let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
+
+let api = Axios.create({
+  baseURL: base + "api/",
+  timeout: 3000,
+  withCredentials: true
+})
+
 
 // let query = new random ([chicken, beef, dfasf, fsdfaf])
 const _foodapi = axios.create({
@@ -11,12 +23,52 @@ const _foodapi = axios.create({
 // const server = baseUrl: localhost 
 export default new Vuex.Store({
   state: {
-
+    user: {}
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user
+    },
+    resetState(state, user) {
+      state.user = {}
+    }
+
+
+
 
   },
   actions: {
+    //#region -- AUTH STUFF --
+    async register({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Register(creds)
+        commit('setUser', user)
+        router.push({ name: "home" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async login({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Login(creds)
+        commit('setUser', user)
+        router.push({ name: "home" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async logout({ commit, dispatch }) {
+      try {
+
+        let success = await AuthService.Logout()
+        if (!success) { }
+        commit('resetState')
+        router.push({ name: "login" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    //#endregion
 
   }
 })
