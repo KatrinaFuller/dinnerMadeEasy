c0bd57b4731d23626c0b40a91dbadfbd2ccea6b0
Valerie Wiklund <vkwiklund@gmail.com>
2019-09-25 14:01:43 -0600

    started profile page

---
+++ client/src/components/profileNavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="profileNavBar">
+    <nav class="navbar navbar-light bg-light">
+      <form class="form-inline">
+        <button class="btn btn-outline-success" type="button" @click="backButton">Back</button>
+        <button class="btn btn-sm btn-outline-secondary" type="button" @click="logout">Logout</button>
+      </form>
+    </nav>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "profileNavBar",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    backButton() {},
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  },
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
