b3685ec64253cef62f6b51ea297bf86d03d77249
Stephanie Hooker <stephanieh799@gmail.com>
2019-09-25 10:58:43 -0600

    fixed store

--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
 import router from './router'
-import AuthService from './AuthService'
+import AuthModule from './modules/AuthModule'
 
 
 Vue.use(Vuex)
 // let query = new random ([chicken, beef,]) this will be for randomly generating our query in the baseUrl
 const _foodapi = Axios.create({
-  baseUrl: 'https://api.edamam.com/search?q=chicken&app_id=$e5152851app_key=$b5e235b115bbed4de5e65e86ac731f4a'
+  baseURL: 'https://api.edamam.com/search?q=chicken&app_id=$e5152851app_key=$b5e235b115bbed4de5e65e86ac731f4a'
 })
 
 let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
@@ -21,6 +21,10 @@ let api = Axios.create({
 
 // const server = baseUrl: localhost 
 export default new Vuex.Store({
+  modules: {
+    AuthModule
+  },
+
   state: {
     user: {}
   },
