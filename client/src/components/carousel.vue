664784638fa243d38241f34e86724a9515ca52dc
Stephanie Hooker <stephanieh799@gmail.com>
2019-09-25 10:48:55 -0600

    resoving merge conflicts

--- client/src/components/carousel.vue
+++ client/src/components/carousel.vue
@@ -1,37 +1,50 @@
 <template>
   <div class="carousel">
     <div class="container text-center my-3">
-      <h2>Bootstrap 4 Multiple Item Carousel</h2>
-      <div class="row mx-auto my-auto">
-        <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
-          <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-              <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=1" />
+      <div class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="row">
+              <div class="col">
+                <img
+                  src="https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
+                  alt="1 slide"
+                />
+              </div>
+              <div class="col">
+                <img
+                  src="https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg"
+                  alt="2 slide"
+                />
+              </div>
+              <div class="col">
+                <img
+                  src="https://www.edamam.com/web-img/4d9/4d9084cbc170789caa9e997108b595de.jpg"
+                  alt="3 slide"
+                />
+              </div>
             </div>
-            <div class="carousel-item">
-              <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=2" />
-            </div>
-            <div class="carousel-item">
-              <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=3" />
-            </div>
-            <div class="carousel-item">
-              <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=4" />
-            </div>
-            <div class="carousel-item">
-              <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=5" />
-            </div>
-            <div class="carousel-item">
-              <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=6" />
+            <div class="row">
+              <div class="col">
+                <img
+                  src="https://www.edamam.com/web-img/2ca/2ca946a40338e9b93c1d14dec518e1b8.jpg"
+                  alt="4 slide"
+                />
+              </div>
+              <div class="col">
+                <img
+                  src="https://www.edamam.com/web-img/fd1/fd1afed1849c44f5185720394e363b4e.jpg"
+                  alt="5 slide"
+                />
+              </div>
+              <div class="col">
+                <img
+                  src="https://www.edamam.com/web-img/8f8/8f810dfe198fa3e520d291f3fcf62bbf.jpg"
+                  alt="6 slide"
+                />
+              </div>
             </div>
           </div>
-          <a class="carousel-control-prev" href="#recipeCarousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#recipeCarousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
         </div>
       </div>
     </div>
--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
+import AuthService from './AuthService'
+
 
 Vue.use(Vuex)
 // let query = new random ([chicken, beef,]) this will be for randomly generating our query in the baseUrl
-const _foodapi = axios.create({
+const _foodapi = Axios.create({
   baseUrl: 'https://api.edamam.com/search?q=chicken&app_id=$e5152851app_key=$b5e235b115bbed4de5e65e86ac731f4a'
+})
+
+let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
+
+let api = Axios.create({
+  baseURL: base + "api/",
+  timeout: 3000,
+  withCredentials: true
 })
 
 // const server = baseUrl: localhost 
 export default new Vuex.Store({
   state: {
-
+    user: {}
   },
   mutations: {
-<<<<<<< HEAD
-=======
     setUser(state, user) {
       state.user = user
     },
@@ -38,11 +48,9 @@ export default new Vuex.Store({
         console.error(error)
       }
     }
-  }
->>>>>>> 0247f363988093a7191370c51b2bfa0aaa7230bd
-
   },
-  actions: {
 
-  }
+
+
+
 })
