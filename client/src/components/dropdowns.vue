0247f363988093a7191370c51b2bfa0aaa7230bd
Katrina Fuller <trinafuller@att.net>
2019-09-25 10:17:02 -0600

    added vt to all home page components

---
+++ client/src/components/dropdowns.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dropdowns"></div>
+</template>
+
+
+<script>
+export default {
+  name: "dropdowns",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
---
+++ client/src/components/homeHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="homeHeader"></div>
+</template>
+
+
+<script>
+export default {
+  name: "homeHeader",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
