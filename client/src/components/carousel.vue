5a4d5fb15480451db75aadaca2326280341fe003
Valerie Wiklund <vkwiklund@gmail.com>
2019-09-25 12:07:34 -0600

    carousel sort of working

--- client/src/components/carousel.vue
+++ client/src/components/carousel.vue
@@ -57,59 +57,71 @@
       </div>
     </div>
   </div>-->
-  <div class="row">
-    <div class="carousel main-carousel slide w-100" data-interval="false" data-ride="carousel">
-      <div class="carousel-inner">
-        <div row class="carousel-item h100 caro-item-1 active">
-          <div class="h-100 d-flex justify-content-center align-items-center">
-            <div class="col-4">
-              <img
-                src="https://www.edamam.com/web-img/8f8/8f810dfe198fa3e520d291f3fcf62bbf.jpg"
-                alt="6 slide"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item h100 caro-item-2">
-          <div class="h-100 d-flex justify-content-end align-items-center">
-            <div class="col-4">
-              <img
-                src="https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
-                alt="1 slide"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item h100 caro-item-3">
-          <div class="h-100 d-flex justify-content-end align-items-center">
-            <div class="col-4">
-              <img
-                src="https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg"
-                alt="2 slide"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item h100 caro-item-4">
-          <div class="h-100 d-flex justify-content-start align-items-center">
-            <div class="col-4">
-              <img
-                src="https://www.edamam.com/web-img/4d9/4d9084cbc170789caa9e997108b595de.jpg"
-                alt="3 slide"
-              />
-            </div>
-          </div>
-        </div>
+  <div class="carousel main-carousel slide w-100" data-ride="carousel">
+    <div row class="carousel-inner">
+      <div class="carousel-item h100 caro-item-1 active">
+        <!-- <div class="col-4"> -->
+        <img
+          class="mx-2"
+          src="https://www.edamam.com/web-img/8f8/8f810dfe198fa3e520d291f3fcf62bbf.jpg"
+          alt="6 slide"
+        />
+        <!-- </div>
+
+        <div class="carousel-item h100 caro-item-2">-->
+        <!-- <div class="col-4"> -->
+        <img
+          class="mx-2"
+          src="https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
+          alt="1 slide"
+        />
+        <!-- </div>
+
+        <div class="carousel-item h100 caro-item-3">-->
+        <!-- <div class="col-4"> -->
+        <img
+          class="mx-2"
+          src="https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg"
+          alt="2 slide"
+        />
       </div>
-      <a class="carousel-control-prev" href=".main-carousel" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href=".main-carousel" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+      <div class="carousel-item h100 caro-item-1">
+        <!-- <div class="col-4"> -->
+        <img
+          class="mx-2"
+          src="https://www.edamam.com/web-img/8f8/8f810dfe198fa3e520d291f3fcf62bbf.jpg"
+          alt="6 slide"
+        />
+        <!-- </div>
+
+        <div class="carousel-item h100 caro-item-2">-->
+        <!-- <div class="col-4"> -->
+        <img
+          class="mx-2"
+          src="https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
+          alt="1 slide"
+        />
+        <!-- </div>
+
+        <div class="carousel-item h100 caro-item-3">-->
+        <!-- <div class="col-4"> -->
+        <img
+          class="mx-2"
+          src="https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg"
+          alt="2 slide"
+        />
+      </div>
     </div>
+    <!-- </div>
+    </div>-->
+    <a class="carousel-control-prev" href=".main-carousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href=".main-carousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 </template>
 
@@ -128,4 +140,7 @@ export default {
 
 
 <style scoped>
+.carousel-control-prev {
+  color: black;
+}
 </style>
