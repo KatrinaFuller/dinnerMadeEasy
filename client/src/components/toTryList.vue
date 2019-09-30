dd4dce2c159fba81ed06d8f5735634575e7599b1
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-09-29 19:08:51 -0600

    added toTry component

---
+++ client/src/components/toTryList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="toTryList">
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'toTryList',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
