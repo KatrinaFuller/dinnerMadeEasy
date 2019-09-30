fe857df6b02aa191f917f8a559f00fd8ac234d2d
Stephanie Hooker <stephanieh799@gmail.com>
2019-09-30 11:15:45 -0600

    added go to recipe button

--- client/src/components/randomRecipeButton.vue
+++ client/src/components/randomRecipeButton.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="randomRecipeButton pt-4">
     <button type="button" class="btn btn-primary btn-lg" @click="generate">Generate Recipe</button>
-
-    <div class="card" style="width: 30rem;" v-if="recipe">
-      <h1>{{recipe.label}}</h1>
-      <img alt="Card image cap" v-bind:src="`${recipe.image}`" />
-      <div class="card-body">
-        <h4>
-          <b>Ingredients:</b>
-        </h4>
-        <p
-          class="card-text"
-          v-for="ingredient in recipe.ingredients"
-          :key="ingredient._id"
-        >{{ingredient.text}}</p>
-        <button class="btn btn-primary" @click="addToFavorites()">Add to My Favorites</button>
-        <button class="btn btn-primary" @click="addToTry">Recipe to try</button>
+    <div class="row justify-content-center pt-4">
+      <div class="card" style="width: 30rem;" v-if="recipe">
+        <h1>{{recipe.label}}</h1>
+        <img alt="Card image cap" v-bind:src="`${recipe.image}`" />
+        <div class="card-body">
+          <h4>
+            <b>Ingredients:</b>
+          </h4>
+          <p
+            class="card-text"
+            v-for="ingredient in recipe.ingredients"
+            :key="ingredient._id"
+          >{{ingredient.text}}</p>
+          <button class="btn btn-primary mx-1" @click="addToFavorites()">Add to My Favorites</button>
+          <button class="btn btn-primary mx-1" @click="addToTry">Recipe to try</button>
+          <button class="btn btn-primary mx-1">
+            <a v-bind:href="`${recipe.url}`" target="_blank">Go To Recipe</a>
+          </button>
+        </div>
       </div>
     </div>
   </div>
