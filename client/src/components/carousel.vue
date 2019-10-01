c2c61ab999aa1283466f8223e4c14235159885f2
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-10-01 14:22:56 -0600

    carousel done

--- client/src/components/carousel.vue
+++ client/src/components/carousel.vue
@@ -1,120 +1,53 @@
 <template>
-  <!-- <div class="carousel">
-    <div class="container text-center my-3">
-      <div class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="row">
-              <div class="col">
-                <img
-                  src="https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
-                  alt="1 slide"
-                />
-              </div>
-              <div class="col">
-                <img
-                  src="https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg"
-                  alt="2 slide"
-                />
-              </div>
-              <div class="col">
-                <img
-                  src="https://www.edamam.com/web-img/4d9/4d9084cbc170789caa9e997108b595de.jpg"
-                  alt="3 slide"
-                />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <img
-                  src="https://www.edamam.com/web-img/2ca/2ca946a40338e9b93c1d14dec518e1b8.jpg"
-                  alt="4 slide"
-                />
-              </div>
-              <div class="col">
-                <img
-                  src="https://www.edamam.com/web-img/fd1/fd1afed1849c44f5185720394e363b4e.jpg"
-                  alt="5 slide"
-                />
-              </div>
-              <div class="col">
-                <img
-                  src="https://www.edamam.com/web-img/8f8/8f810dfe198fa3e520d291f3fcf62bbf.jpg"
-                  alt="6 slide"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href=".main-carousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href=".main-carousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-  </div>-->
   <div class="carousel main-carousel slide w-100" data-ride="carousel">
     <div row class="carousel-inner">
       <div class="carousel-item h100 caro-item-1 active">
-        <!-- <div class="col-4"> -->
+        <a href="http://www.bbcgoodfood.com/recipes/2558/" target="_blank">
         <img
           class="mx-2"
-          src="https://www.edamam.com/web-img/8f8/8f810dfe198fa3e520d291f3fcf62bbf.jpg"
+          src="https://www.edamam.com/web-img/d37/d376c145f2a59befa7738a2c35caab31.jpg"
           alt="6 slide"
-        />
-        <!-- </div>
-
-        <div class="carousel-item h100 caro-item-2">-->
-        <!-- <div class="col-4"> -->
-
+          />
+        </a>
+        <a href="http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html" target="_blank">
         <img
           class="mx-2"
           src="https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
           alt="1 slide"
         />
-        <!-- </div>
-
-        <div class="carousel-item h100 caro-item-3">-->
-        <!-- <div class="col-4"> -->
+        </a>
+        <a href="http://nymag.com/restaurants/articles/recipes/fishsteak.htm" target="_blank">
         <img
           class="mx-2"
-          src="https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg"
+          src="https://www.edamam.com/web-img/9be/9be37985e53b2158a974437a959a9e2d.jpg"
           alt="2 slide"
         />
+        </a>
       </div>
-      <div class="carousel-item h100 caro-item-1">
-        <!-- <div class="col-4"> -->
+      <div class="carousel-item h100 caro-item-1 active">
+        <a href="https://www.epicurious.com/recipes/food/views/baked-gefilte-fish-51154400" target="_blank">
         <img
           class="mx-2"
-          src="https://www.edamam.com/web-img/8f8/8f810dfe198fa3e520d291f3fcf62bbf.jpg"
+          src="https://www.edamam.com/web-img/30b/30bc8ba581e8df430cff2a22c5e32abf.jpg"
           alt="6 slide"
         />
-        <!-- </div>
-
-        <div class="carousel-item h100 caro-item-2">-->
-        <!-- <div class="col-4"> -->
+        </a>
+        <a href="http://www.bbc.co.uk/food/recipes/roastleglessturkey_13963" target="_blank">
         <img
           class="mx-2"
-          src="https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg"
+          src="https://www.edamam.com/web-img/a92/a92d1edb41681ffd984ed2ca7eeb97ec.jpg"
           alt="1 slide"
         />
-        <!-- </div>
-
-        <div class="carousel-item h100 caro-item-3">-->
-        <!-- <div class="col-4"> -->
+        </a>
+        <a href="http://honestcooking.com/pig-of-the-week-rillette-recipe/" target="_blank">
         <img
           class="mx-2"
-          src="https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg"
+          src="https://www.edamam.com/web-img/d23/d23884c327c2bb711065ece7be236612.jpg"
           alt="2 slide"
         />
+        </a>
       </div>
     </div>
-    <!-- </div>
-    </div>-->
     <a class="carousel-control-prev" href=".main-carousel" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon"></span>
       <span class="sr-only">Previous</span>
