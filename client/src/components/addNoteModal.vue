3cd23c7c9b26eb428aad1c137a113ab62a0b0bb3
Katrina Fuller <trinafuller@att.net>
2019-10-01 12:10:49 -0600

    addNoteModal created and working

---
+++ client/src/components/addNoteModal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="add-note-modal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Add Note</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form @submit.prevent="addNote()">
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                id="note"
+                placeholder="Enter Note"
+                v-model="newNote.notes"
+                required
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "add-note-modal",
+  data() {
+    return {
+      newNote: {}
+    };
+  },
+  computed: {},
+  methods: {
+    addNote() {
+      this.newNote.recipeId = this.recipeId;
+      this.$store.dispatch("addNote", this.newNote);
+      $(".close").click();
+    }
+  },
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
