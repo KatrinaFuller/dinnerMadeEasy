439504153f6c15d17458055fa351fc1277c812d5
Katrina Fuller <trinafuller@att.net>
2019-09-30 20:09:45 -0600

    started getting activeRecipe view set up

--- client/src/views/ActiveRecipe.vue
+++ client/src/views/ActiveRecipe.vue
@@ -5,19 +5,35 @@
         <button class="btn btn-outline-primary" type="button" @click="backToProfile">Back to Profile</button>
       </div>
     </div>
+    <div class="row" v-if="recipe">
+      <h1>Title: {{recipe.label}}</h1>
+      <img v-bind:src="`${recipe.image}`" />
+      <h4>
+        <b>Ingredients:</b>
+      </h4>
+      <p v-for="ingredient in recipe.ingredients" :key="ingredient._id">{{ingredient.text}}</p>
+    </div>
   </div>
 </template>
 
 
 <script>
-import router from "@/router.js";
+// import router from "@/router.js";
 
 export default {
   name: "activeRecipe",
+  // props: ["recipe"],
   data() {
     return {};
   },
-  computed: {},
+  computed: {
+    recipe() {
+      return this.$store.state.recipes.recipe;
+    },
+    ingredient() {
+      return this.$store.state.recipes.recipe.ingredients;
+    }
+  },
   methods: {
     backToProfile() {
       this.$store.dispatch("backToProfile");
