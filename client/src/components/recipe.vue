afeddccbd4c56ed0bb9017b1b30f0d07d766e7c6
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-10-11 16:06:16 -0600

    hover effect

--- client/src/components/recipe.vue
+++ client/src/components/recipe.vue
@@ -70,4 +70,11 @@ export default {
 .x {
   color: #cb3232;
 }
+
+.card:hover {
+  transform: scale(0.9, 0.9);
+  box-shadow: 5px 5px 30px 15px rgba(0,0,0,0.25), 
+    -5px -5px 30px 15px rgba(0,0,0,0.22);
+  cursor: pointer;
+}
 </style>
