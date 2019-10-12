316f6ea77045a4771bdb639d9987d121cc3899ce
Katrina Fuller <trinafuller@att.net>
2019-10-12 01:14:54 -0600

    works?

---
+++ client/dist/precache-manifest.435d882ebb226011eb31e4a35e5338b5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4137d0dcadf64b3bd320",
+    "url": "/css/MoreFavRecipes.7fa756cd.css"
+  },
+  {
+    "revision": "4137d0dcadf64b3bd320",
+    "url": "/js/MoreFavRecipes.8ef022c3.js"
+  },
+  {
+    "revision": "851c816ae8e098fbdc4f",
+    "url": "/css/MoreToTry.a877434f.css"
+  },
+  {
+    "revision": "851c816ae8e098fbdc4f",
+    "url": "/js/MoreToTry.704a3d98.js"
+  },
+  {
+    "revision": "71d74549996a34316ca8",
+    "url": "/css/activeRecipe.cb52bf30.css"
+  },
+  {
+    "revision": "71d74549996a34316ca8",
+    "url": "/js/activeRecipe.351a1abf.js"
+  },
+  {
+    "revision": "f46c8ebf093a7431c844",
+    "url": "/css/app.553d6eab.css"
+  },
+  {
+    "revision": "f46c8ebf093a7431c844",
+    "url": "/js/app.77448400.js"
+  },
+  {
+    "revision": "6c46d63bcb90639a83d7",
+    "url": "/js/chunk-vendors.81ce4812.js"
+  },
+  {
+    "revision": "b7118de46913afba108a",
+    "url": "/css/profile.772ae384.css"
+  },
+  {
+    "revision": "b7118de46913afba108a",
+    "url": "/js/profile.4c6a0b7a.js"
+  },
+  {
+    "revision": "e68538e51362b813fb49aa17afde5de3",
+    "url": "/img/dmelogo.e68538e5.png"
+  },
+  {
+    "revision": "376afabef4884cbf51ad9d47aa1cf917",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca86cf4ce2cabfa2f2ddba749ad15f1",
+    "url": "/tsmchefs.ico"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "192b46b288bc5b361710623a38c8de03",
+    "url": "/tsmchefs.png"
+  },
+  {
+    "revision": "f194429eeb5d74f7389a3f3631b1b192",
+    "url": "/whiteSmChefs.png"
+  }
+];
